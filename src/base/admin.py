13957d52bae5def7a5cbc2456929177f89be6227
S Jansen <samuelanton@hotmail.com>
2015-07-25 16:47:32 +0200

    Created custom form for Family (now you can directly add FamilyMembers)

--- src/base/admin.py
+++ src/base/admin.py
@@ -6,7 +6,17 @@ admin.site.register(Event)
 admin.site.register(Slide)
 admin.site.register(Service)
 admin.site.register(TimetableDuty)
-admin.site.register(Address)
 admin.site.register(Profile)
-admin.site.register(Family)
 admin.site.register(FamilyMember)
+admin.site.register(Address)
+
+
+# Create custom form for Family
+class FamilyMemberInline(admin.StackedInline):
+  model = FamilyMember
+  extra = 0
+
+class FamilyAdmin(admin.ModelAdmin):
+  inlines = [FamilyMemberInline]
+
+admin.site.register(Family, FamilyAdmin)
