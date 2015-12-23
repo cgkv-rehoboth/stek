7190c33051e48af920786346c488541272d6c931
Samuel-Anton <samuelanton@hotmail.com>
2015-12-23 19:44:13 +0100

    Roosters verbeterd -> current user toegevoegd en 'kies een rooster' als user geen rooster heeft

--- src/agenda/views.py
+++ src/agenda/views.py
@@ -9,14 +9,17 @@ from .models import *
 
 @login_required
 def timetables(request):
-  table_id = 1
+
+  mytables = Timetable.objects.filter(team__members__pk=request.user.pk).exclude(team__isnull=True)
+  notmytables = Timetable.objects.exclude(team__members__pk=request.user.pk).exclude(team__isnull=True)
+
+  table_id = 0
+  if mytables.exists():
+    table_id = mytables[0].pk
   if request.GET and request.GET['table']:
     table_id = int(request.GET['table'])
 
   duties = TimetableDuty.objects.filter(timetable=table_id, event__startdatetime__gte=datetime.date.today()).order_by("event__startdatetime", "event__enddatetime")
-
-  mytables = Timetable.objects.filter(team__members__pk=request.user.pk).exclude(team__isnull=True)
-  notmytables = Timetable.objects.exclude(team__members__pk=request.user.pk).exclude(team__isnull=True)
 
   return render(request, 'timetables.html', {
     'table_id': table_id,
