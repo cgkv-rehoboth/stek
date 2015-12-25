da6e25b81df86df8fd91fda60e2ad3c22018d2f4
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2015-12-25 16:57:09 +0100

    Fixed urls

--- src/cgkv/urls.py
+++ src/cgkv/urls.py
@@ -9,14 +9,19 @@ import agenda.views
 import agenda.api
 import public.views
 
+apipatterns = patterns('',
+  url(r'^agenda/', include(agenda.api.urls)),
+)
+
 urlpatterns = patterns('',
   url(r'^', include(base.views.urls)),
   url(r'^', include(agenda.views.urls)),
-  url(r'^', include(agenda.api.urls)),
   url(r'^', include(public.views.urls)),
 
-  url(r'^admin/', include(admin.site.urls)),
-  url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
+  url(r'^api/v1/', include(apipatterns)),
 
-  url(r'^', include(static.static("/", document_root=os.path.join(settings.BASE_DIR, "static/")))),
+  url(r'^admin/', include(admin.site.urls)),
+  # url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
+
+  # url(r'^', include(static.static("/", document_root=os.path.join(settings.BASE_DIR, "static/")))),
 )
