3a6e7924a51cb7a751eb7fdfe27561e28c9182dc
Samuel-Anton <samuelanton@hotmail.com>
2015-12-26 12:27:58 +0100

    Now extra template modules can be added and used

--- src/cgkv/settings.py
+++ src/cgkv/settings.py
@@ -17,6 +17,22 @@ TEMPLATE_LOADERS = (
     )),
 )
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
