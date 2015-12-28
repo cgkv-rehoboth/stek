4817fd2d3a42593c18dcde500a356d4fbb40c476
Samuel-Anton <samuelanton@hotmail.com>
2015-12-28 14:20:50 +0100

    added django_tables2 to INSTALLED_APPS

--- src/cgkv/settings.py
+++ src/cgkv/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = (
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
+  'django_tables2',
   'rest_framework',
   'django_extensions',
   'corsheaders',
