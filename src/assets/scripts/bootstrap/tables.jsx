14b4a8d8017272bee373023be4619ef02b9007f4
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2015-12-29 15:24:01 +0100

    Scaffolding bootstrap tables

---
+++ src/assets/scripts/bootstrap/tables.jsx
@@ -0,0 +1,26 @@
+let React = require("react");
+let _ = require('underscore');
+
+class TRow extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+class TCell extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+class Table extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+module.exports = {
+  TRow: TRow,
+  TCell: TCell,
+  Table, Table
+};
