08b63086671835c5f33420d829b1d0c559270d9b
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2015-12-30 18:37:04 +0100

    SearchTable

---
+++ src/assets/scripts/ProfileSearchTable.jsx
@@ -0,0 +1,61 @@
+let React = require("react");
+let _ = require('underscore');
+let api = require('api');
+let forms = require('forms');
+let { Table } = require('bootstrap/tables');
+
+class ProfileSearchTable extends React.Component {
+
+  static get propTypes() {
+    return {};
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: []
+    };
+  }
+
+  componentWillMount() {
+    // load initial profiles
+    api.profiles.list()
+      .then((data) => {
+        console.debug("Loading profiles complete");
+
+        this.setState({
+          profiles: data.data.results
+        });
+      });
+  }
+
+  render() {
+    let rows = _.map(this.state.profiles, (prof) => {
+      var family;
+      if(prof.family) {
+        family = <a href={"/adresboek/families/" + prof.family.id + "/"}>{prof.user.last_name}</a>;
+      } else {
+        family = <span></span>;
+      }
+
+      return <tr key={prof.id}>
+        <td><a href={"/adresboek/profile/" + prof.id + "/"}>{prof.user.first_name}</a></td>
+        <td>{family}</td>
+        <td>{prof.address.street} <small>({prof.address.zip})</small></td>
+        <td>{prof.address.city}</td>
+        <td>{prof.phone}</td>
+        <td>{prof.user.email}</td>
+      </tr>;
+    });
+
+    return <div>
+      <Table>
+        <tbody>
+        {rows}
+        </tbody>
+      </Table>
+    </div>;
+  }
+}
+
+module.exports = ProfileSearchTable;
