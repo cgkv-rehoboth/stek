42562c98f464610249dc400360f80a4e382c9613
Samuel-Anton <samuelanton@hotmail.com>
2015-12-30 22:55:39 +0100

    Splitted the .js files for the shared addressbookPersons.html (shared by: persons and favorites)

---
+++ src/assets/scripts/pages/addressbookPersons.js
@@ -0,0 +1,5 @@
+let React = require("react");
+let ReactDom = require("react-dom");
+let ProfileSearchTable = require("ProfileSearchTable");
+
+ReactDom.render(<ProfileSearchTable />, $("#profile-search-table")[0]);
