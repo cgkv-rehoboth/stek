18302643824c1ce0e8115b7391a7dc15fa5ebf1f
Samuel-Anton <samuelanton@hotmail.com>
2016-06-05 16:31:32 +0200

    Added extra info dropdown

--- src/assets/scripts/ServiceTable.jsx
+++ src/assets/scripts/ServiceTable.jsx
@@ -20,15 +20,43 @@ export default class ServiceTable extends React.Component {
     let renderServiceRow = (serv) => {
       let starttime = moment(serv.startdatetime).format('H:mm');
       let startdate = moment(serv.startdatetime).format('dddd D MMMM');
+      let hiddeninfo = serv.minister + (serv.comments && serv.comments.length > 0 ? " - " +serv.comments : "");
+      let html = serv.comments && serv.comments.length > 0 ? <i className="fa fa-info-circle" ariaHidden="true"></i> : "";
 
-      return (
-        <tr key={serv.id}>
+      function onClick(e){
+        e = e || window.e; // Cross-browser solution
+
+        // Get serviceHiddenItems row
+        var tr = $(e.target).closest('tr').next();
+
+        /* Toggle serviceHiddenItems */
+
+        // (A) Only show one at a time
+        if($(tr).css('display') == "none"){
+          $(".serviceHiddenItems").hide();
+          $(tr).show();
+        }else{
+          $(".serviceHiddenItems").hide();
+        }
+
+        /*
+        // (B) Don't bother how many are shown at a time
+        $(tr).toggle();
+        */
+      }
+
+      return ([
+        <tr key={serv.id} className="serviceRow" onClick={onClick.bind(this)}>
           <td>{serv.title}</td>
           <td><span className="service-starttime">{starttime}</span> - {startdate}</td>
-          <td>{serv.minister}</td>
-          <td>{serv.comments}</td>
+          <td className="serviceHideOnSmallScreen">{serv.minister}</td>
+          <td className="serviceInfoIcon">{html}</td>
+          <td className="serviceHideOnSmallScreen">{serv.comments}</td>
+        </tr>,
+        <tr className="serviceHiddenItems">
+          <td colSpan="5"><div>{hiddeninfo}</div></td>
         </tr>
-      );
+      ]);
     };
     
     return (
@@ -36,10 +64,51 @@ export default class ServiceTable extends React.Component {
         <tr>
           <th>Dienst</th>
           <th>Datum</th>
-          <th>Voorganger</th>
+          <th className="serviceHideOnSmallScreen">Voorganger</th>
           <th></th>
+          <th className="serviceHideOnSmallScreen"></th>
         </tr>
       </SearchTable>
     );
   }
 }
+
+/*
+
+export class ServiceRow extends React.Component {
+
+  static get propTypes() {
+    return {
+      service: React.PropTypes.object.isRequired // (searchText, page) => promise<[item]>,
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    console.log("Constructed 2");
+    this.state = {
+      fold: true,
+    };
+  }
+
+  onClick(e){
+    console.log("Clicked 2");
+    this.setState({
+      fold: !this.state.fold
+    });
+  }
+
+  render() {
+    let starttime = moment(service.startdatetime).format('H:mm');
+    let startdate = moment(service.startdatetime).format('dddd D MMMM');
+    return (
+      <tr key={service.id} onClick={this.onClick.bind(this)}>
+        <td>{service.title}</td>
+        <td><span className="service-starttime">{starttime}</span> - {startdate}</td>
+        <td>{service.minister}</td>
+        <td>{service.comments}</td>
+      </tr>
+    );
+  }
+}
+  */
