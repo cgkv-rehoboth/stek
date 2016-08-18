806942e21b79ee33df8f6772538f747363d9421c
Samuel-Anton <samuelanton@hotmail.com>
2016-08-18 09:19:04 +0200

    Edit basic profile data

--- src/base/views.py
+++ src/base/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 from django.db.models import Prefetch, Q
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.decorators.http import *
 from django.conf.urls import patterns, include, url
 from django import http
 
@@ -65,6 +67,38 @@ def profile_detail(request, pk):
     'memberships': memberships
   })
 
+@login_required
+def profile_detail_edit(request, pk):
+  profile = Profile.objects.get(pk=pk)
+
+  print(request.profile.pk)
+  print(profile.pk)
+  if not request.profile.pk == profile.pk:
+    return HttpResponse(status=404)
+
+  return render(request, 'profile_edit.html', {
+    'p': profile,
+  })
+
+@login_required
+@require_POST
+def profile_detail_edit_save(request, pk):
+  profile = Profile.objects.get(pk=pk)
+
+  if not request.profile.pk == profile.pk:
+    return HttpResponse(status=404)
+
+  profile.first_name = request.POST.get("first_name", "")
+  profile.last_name = request.POST.get("last_name", "")
+  profile.birthday = request.POST.get("birthday", "")
+  profile.email = request.POST.get("email", "")
+  profile.phone = request.POST.get("phone", "")
+
+  profile.save()
+
+  return redirect('profile-detail-page', pk=pk)
+
+
 urls = [
   url(r'^login$', auth_views.login, {'template_name':'login.html'}, name='login'),
   url(r'^adresboek/$', profile_list, name='profile-list-page'),
@@ -73,5 +107,8 @@ urls = [
   url(r'^adresboek/families/(?P<pk>\d+)/$', family_list, name='family-detail-page'),
   url(r'^teams/$', team_list, name='team-list-page'),
   url(r'^teams/(?P<pk>\d+)/$', team_list, name='team-detail-page'),
+
+  url(r'^profiel/(?P<pk>\d+)/edit/save/$', profile_detail_edit_save, name='profile-detail-page-edit-save'),
+  url(r'^profiel/(?P<pk>\d+)/edit/$', profile_detail_edit, name='profile-detail-page-edit'),
   url(r'^profiel/(?P<pk>\d+)/$', profile_detail, name='profile-detail-page'),
 ]
