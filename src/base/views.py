c89c8df5d7c82cfe9cf4b2857fc749d5412b6a39
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2016-09-11 15:47:06 +0200

    Fixed password reset complete url

--- src/base/views.py
+++ src/base/views.py
@@ -73,7 +73,7 @@ urls = [
   }, name='password_reset'),
   url(r'^wachtwoord_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
   url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-  url(r'^reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+  url(r'^reset/done/$', auth_views.password_reset_done, name='password_reset_complete'),
 
   # addressbook
   url(r'^adresboek/$', profile_list, name='profile-list-page'),
