cc2ac404bb0126fa55eba8ead944380c25ad2ecd
Samuel-Anton <samuelanton@hotmail.com>
2016-10-05 19:52:01 +0200

    Made username generator nicer Added option to invinte only the Chosen Ones (profile ID's need to be hardcoded)

--- src/base/management/commands/spawn_accounts.py
+++ src/base/management/commands/spawn_accounts.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from base.models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordResetForm
+import unidecode
 
 class NewAccountPasswordResetForm(PasswordResetForm):
   def send_mail(self, subject_template_name, email_template_name, context, from_email, to_email, html_email_template_name=None):
@@ -39,15 +40,32 @@ class Command(BaseCommand):
 
   def add_arguments(self, parser):
     parser.add_argument('--dryrun', action='store_true')
+    parser.add_argument('--chosenones', action='store_true')
 
   def handle(self, *args, **options):
     dryrun = options['dryrun']
+    chosenones = options['chosenones']
     if dryrun:
       print(">> Dry-run: only reporting")
       print()
       dryrun = True
 
-    profiles = Profile.objects.all()
+    if chosenones:
+      users = [
+        #386, # André Jansen
+        228, # Corné Mostert
+        #145, # René van Bruggen
+        #319, # Bram Steenbrink
+        #130, # Willeke Boer-de Hoop
+        #377, # Eric Reijm
+        #196, # Max van der Stoel
+        #366, # Anne Schouls
+        #143, # Larissa van der Wal
+      ]
+
+      profiles = Profile.objects.filter(pk__in=users)
+    else:
+      profiles = Profile.objects.all()
 
     for prof in collect_accountless(profiles):
       # ensure an email account is set
@@ -55,8 +73,14 @@ class Command(BaseCommand):
         print("[FAILURE] Profile '%s %s' has no account, but no email address is set!" % (prof.first_name, prof.last_name))
         continue
 
-      # think of a clever username
-      username = "%s.%s" % (prof.first_name, prof.last_name)
+      ## think of a clever username
+      # Remove all non alphabatic and special chars
+      firstname = ''.join(e for e in unidecode.unidecode(prof.first_name.lower()) if e.isalnum())
+      lastname = prof.last_name.split('-').pop()
+      lastname = ''.join(e for e in unidecode.unidecode(lastname.lower()) if e.isalnum())
+
+      # Merge it for an username
+      username = "%s.%s" % (firstname, lastname)
 
       # ensure the username is not taken
       if User.objects.filter(username=username).exists():
