d4efb9066113742899ce556932b8e0f2647baea0
Samuel-Anton Jansen <sa.jansen1@gmail.com>
2017-02-18 11:59:24 +0100

    small error fix

--- src/cgkv/settings.py
+++ src/cgkv/settings.py
@@ -249,7 +249,7 @@ CKEDITOR_CONFIGS = {
     ],
     'width'         : '100%',
     # 'allowedContent': ['p', 'strong', 'i', 's', 'u', 'span', 'div', 'ul', 'li', 'ol', 'em', 'a', 'table', 'thead', 'tbody', 'caption', 'tr', 'td', 'th', 'img', 'br', 'hr', 'pr', 'address', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
-  }
+  },
   'no_files': {
     'toolbar'       : 'Customno_files',
     'toolbar_Customno_files': [
